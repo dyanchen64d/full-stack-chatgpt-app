089a531143276c045b5563ce39a52f875ec4c13b
chen.zhang <zhangchen64d@gmail.com>
2023-02-05 15:10:55 +0900

    update

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5001', {
+  const response = await fetch('https://full-stack-chatgpt-app.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
